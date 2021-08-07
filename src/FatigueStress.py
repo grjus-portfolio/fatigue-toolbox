9595590325268df8cbbaace68ace554f1148dc37
grjus@pm.me <grjus@pm.me>
2021-08-07 20:34:42 +0000

    Added Gerber model for fatigue evaluation

--- src/FatigueStress.py
+++ src/FatigueStress.py
@@ -8,3 +8,14 @@ class FatigueStress(ABC):
     @abstractmethod
     def calculate(self)->list[float]:
         pass
+
+
+
+class GerberStress(FatigueStress):
+    def __init__(self, alternating_stress: list[float], mean_stress: list[float], ultimate_strength:float):
+        super().__init__(alternating_stress, mean_stress)
+        self.ultimate_strength = ultimate_strength
+
+    def calculate(self) -> list[float]:
+        result = map(lambda alt, mean:alt/(1-mean/self.ultimate_strength)**2, self.alternating_stress, self.mean_stress)
+        return list(result)
